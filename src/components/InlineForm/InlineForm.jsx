6259fc7abbf031da7f365a5478fdd40af65441ff
StateraMentis <111475960+StateraMentis@users.noreply.github.com>
2023-07-27 17:26:19 +0700

    7

--- src/components/InlineForm/InlineForm.jsx
+++ src/components/InlineForm/InlineForm.jsx
@@ -55,7 +55,7 @@ const Form = () => {
 
     return (
         <div className={"form"}>
-            <h3>Введите ваши данные</h3>
+            <h3>Инлайн форма</h3>
             <input
                 className={'input'}
                 type="text"
