339d2dbaa970396da56625f17c4c1cf6cd06b355
StateraMentis <111475960+StateraMentis@users.noreply.github.com>
2023-07-27 17:05:03 +0700

    4

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
 import {Route, Routes} from 'react-router-dom'
-import ProductList from "./components/InlineForm/InlineForm";
+import InlineForm from "./components/InlineForm/InlineForm";
 import Form from "./components/Form/Form";
 
 function App() {
