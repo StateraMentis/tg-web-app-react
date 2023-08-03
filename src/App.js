01e780f45608b8b54a2a8211f73fe91d6251b5c3
StateraMentis <111475960+StateraMentis@users.noreply.github.com>
2023-08-03 13:41:51 +0700

    333

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
     return (
         <div className="App">
-            <Header />
+            {/* <Header /> */}
             <Routes>
                 <Route index element={<Form />}/>
                 <Route path={'inline'} element={<InlineForm />}/>
